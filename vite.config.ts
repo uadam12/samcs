e847031b0a7f61fc9feef3569658ac204f726585
Usman Adam Imam <uadam12@gmail.com>
2026-06-07 13:09:01 +0100

    handle base url for vercel and github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  'base': "/samcs/",
+  'base': process.env.VERCEL ? '/' : '/samcs/',
   plugins: [
     react(), tailwindCSS()
   ],
